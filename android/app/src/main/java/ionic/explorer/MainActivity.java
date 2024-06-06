8be579bef2aa5a4532f2f0bce9f36a70abaf06dd
Gab-Delzo <162981653+Gab-Delzo@users.noreply.github.com>
2024-06-06 11:39:10 -0400

    agregar archivos

---
+++ android/app/src/main/java/ionic/explorer/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.explorer;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.explorer',
+  appName: 'explorer',
+  webDir: 'www'
+};
+
+export default config;
